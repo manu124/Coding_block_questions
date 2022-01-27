4037d29264540221277b53e6187547c25c90b077
manub <manu.bhardwaj_cs.da19@gla.ac.in>
2022-01-27 11:54:30 +0530

    code for stock span

--- src/Stock_Span_Stack.java
+++ src/Stock_Span_Stack.java
@@ -1,3 +1,22 @@
-public class Stock_Span_Stack {
 
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Stock_Span_Stack {
+    public static void main(String[] args) {
+        int[] ht={100,80,60,70,60,75,85};
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        int[] span = new int[ht.length];
+        span[0]=1;
+        for (int cur = 1; cur <ht.length; cur++) {
+            while(!st.isEmpty() && ht[cur]>=ht[st.peek()]){
+                st.pop();
+            }
+            span[cur]=cur-st.peek();
+            st.push(cur);
+        }
+        System.out.println(Arrays.toString(span));
+
+    }
 }
